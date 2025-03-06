88aa0a0f466a691564d801debebb36911f57f03e
workpaperseizer <workpaperseizer@gmail.com>
2025-03-06 10:25:49 -0500

    Edit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("Device Orientation Script Loaded");
 
+// 요청 시점 변경: 페이지 로드 시 권한 요청
+document.addEventListener("DOMContentLoaded", requestMotionPermission);
+
 function handleOrientation(event) {
     let beta = event.beta; // Forward/backward tilt
 
@@ -23,7 +26,7 @@ function handleOrientation(event) {
     }
 }
 
-// Check for permission on iOS
+// iOS에서 자동으로 권한 요청
 function requestMotionPermission() {
     if (typeof DeviceOrientationEvent.requestPermission === "function") {
         DeviceOrientationEvent.requestPermission()
@@ -36,10 +39,7 @@ function requestMotionPermission() {
             })
             .catch(console.error);
     } else {
-        // Non-iOS devices
+        // iOS가 아닐 때 바로 이벤트 등록
         window.addEventListener("deviceorientation", handleOrientation);
     }
 }
-
-// Request permission when user interacts
-document.addEventListener("click", requestMotionPermission);
