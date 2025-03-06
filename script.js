b1cf62b0c09a88e9603fcd41cfb7c7132a60f66e
workpaperseizer <workpaperseizer@gmail.com>
2025-03-06 11:17:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-console.log("!!!");
+console.log("Device Orientation Script Loaded");
 
 // 요청 시점 변경: 페이지 로드 시 권한 요청
 document.addEventListener("DOMContentLoaded", requestMotionPermission);
 
 function handleOrientation(event) {
-    let beta = event.beta; // 앞뒤 기울기
-    let gamma = event.gamma; // 좌우 기울기
+    let beta = event.beta; // 앞뒤 기울기 (-90 ~ 90)
+    let gamma = event.gamma; // 좌우 기울기 (-90 ~ 90)
 
     let topLeft = document.querySelector(".top-left");
     let topRight = document.querySelector(".top-right");
@@ -14,27 +14,17 @@ function handleOrientation(event) {
     let left = document.querySelector(".left");
     let right = document.querySelector(".right");
 
-    // 앞뒤 기울기: 상단, 하단 높이 변경
-    if (beta < -10) {
-        topLeft.style.height = "10vh";
-        topRight.style.height = "10vh";
-        bottomLeft.style.height = "90vh";
-        bottomRight.style.height = "90vh";
-    } else if (beta > 10) {
-        topLeft.style.height = "90vh";
-        topRight.style.height = "90vh";
-        bottomLeft.style.height = "10vh";
-        bottomRight.style.height = "10vh";
-    }
+    // 🔹 `beta` 값 (-90 ~ 90)을 정규화하여 height 변경 (10vh ~ 90vh)
+    let normalizedHeight = ((beta + 90) / 180) * 80 + 10; // 10 ~ 90vh
+    topLeft.style.height = `${normalizedHeight}vh`;
+    topRight.style.height = `${normalizedHeight}vh`;
+    bottomLeft.style.height = `${100 - normalizedHeight}vh`;
+    bottomRight.style.height = `${100 - normalizedHeight}vh`;
 
-    // 좌우 기울기: 좌우 flex 반전
-    if (gamma < -10) {
-        left.style.flex = "0";
-        right.style.flex = "10";
-    } else if (gamma > 10) {
-        left.style.flex = "10";
-        right.style.flex = "0";
-    }
+    // 🔹 `gamma` 값 (-90 ~ 90)을 정규화하여 flex 변경 (0 ~ 10)
+    let normalizedFlex = ((gamma + 90) / 180) * 10; // 0 ~ 10
+    left.style.flex = `${10 - normalizedFlex}`;
+    right.style.flex = `${normalizedFlex}`;
 }
 
 // iOS에서 자동으로 권한 요청
